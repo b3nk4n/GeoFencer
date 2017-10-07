235c1aa677b0d152d54dadd8bb958fd632b9a917
bsautermeister <apps@bsautermeister.de>
2017-10-07 12:44:10 +0200

    Show notification on geofence transition.

--- app/src/main/java/com/example/bsautermeister/geofencer/geo/GeoTransitionReceiver.java
+++ app/src/main/java/com/example/bsautermeister/geofencer/geo/GeoTransitionReceiver.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.example.bsautermeister.geofencer.notification.SimpleNotification;
+import com.example.bsautermeister.geofencer.utils.DateUtils;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
 
 public class GeoTransitionReceiver extends BroadcastReceiver {
     private static final String TAG = "GeoTransitionReceiver";
 
+    SimpleNotification notification;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        notification = new SimpleNotification(context);
 
         if (geofencingEvent != null && !geofencingEvent.hasError()) {
             int transition = geofencingEvent.getGeofenceTransition();
@@ -28,7 +35,20 @@ public class GeoTransitionReceiver extends BroadcastReceiver {
     }
 
     // TODO missing background thread here in Comfylight project?
-    void handleReceivedEventOnBackgroundThread(@NonNull final GeofencingEvent geofencingEvent) {
-        //notificationHandler.handleReceivedTransitionEvent(geofencingEvent, homeId);
+    private void handleReceivedEventOnBackgroundThread(@NonNull final GeofencingEvent geofencingEvent) {
+        String content = String.format("%s: %s", DateUtils.getTimestamptString(), transitionString(geofencingEvent));
+        notification.show(0, content);
+    }
+
+    private String transitionString(@NonNull final GeofencingEvent geofencingEvent) {
+        int transitionCode = geofencingEvent.getGeofenceTransition();
+        if (transitionCode == Geofence.GEOFENCE_TRANSITION_EXIT)
+            return "EXIT";
+        else if (transitionCode == Geofence.GEOFENCE_TRANSITION_ENTER)
+            return "ENTER";
+        else if (transitionCode == Geofence.GEOFENCE_TRANSITION_DWELL)
+            return "DWELL";
+        else
+            return "UNKNOWN";
     }
 }
