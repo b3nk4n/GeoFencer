10cc03edebeccf7c312a20586874ba588abb9dd5
bsautermeister <apps@bsautermeister.de>
2017-10-07 12:43:26 +0200

    Added date utils.

---
+++ app/src/main/java/com/example/bsautermeister/geofencer/utils/DateUtils.java
@@ -0,0 +1,17 @@
+package com.example.bsautermeister.geofencer.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class DateUtils {
+    public static String getTimestamptString() {
+        Calendar calendar = Calendar.getInstance();
+        TimeZone tz = TimeZone.getDefault();
+        calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        java.util.Date currenTimeZone=new java.util.Date((long)1379487711*1000);
+        return sdf.format(currenTimeZone);
+    }
+}
