13fa1253bdd218c827ff7293424b784ec8e38b51
bsautermeister <apps@bsautermeister.de>
2017-10-07 12:28:59 +0200

    Added simple notification utility class.

---
+++ app/src/main/java/com/example/bsautermeister/geofencer/notification/SimpleNotification.java
@@ -0,0 +1,54 @@
+package com.example.bsautermeister.geofencer.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+
+import com.example.bsautermeister.geofencer.R;
+
+public class SimpleNotification {
+
+    private static final String CHANNEL_ID = "com.example.bsautermeister.geofencer";
+
+    private final Context context;
+    private final NotificationManager notificationManager;
+
+    public SimpleNotification(Context appContext) {
+        this.context = appContext;
+        notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        createChannel(CHANNEL_ID, "Geofencer", "Geofencing notifications.");
+    }
+
+    private void createChannel(String id, String name, String description) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(id, name,
+                    NotificationManager.IMPORTANCE_HIGH);
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 100});
+            notificationManager.createNotificationChannel(mChannel);
+        }
+    }
+
+    public void show(int id, String content) {
+        Notification.Builder nb;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            nb  = new Notification.Builder(context, CHANNEL_ID);
+        } else {
+            nb  = new Notification.Builder(context);
+        }
+        Notification notification = nb.setContentTitle("Geofencer")
+                                        .setContentText(content)
+                                        .setSmallIcon(R.mipmap.ic_launcher)
+                                        .setAutoCancel(true).build();
+
+        notificationManager.notify(0, notification);
+    }
+}
